e34ba8f6cb528cca70013bc1214bfbb8086f4f88
VD3baeKy <digfehc@gmail.com>
2025-02-24 16:02:11 +0900

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -82,8 +82,8 @@ CREATE OR REPLACE FUNCTION update_timestamp()
 RETURNS TRIGGER AS $$
 BEGIN
   NEW.updated_at = NOW();
-  RETURN NEW;
-END;
+  RETURN NEW
+END
 $$ language 'plpgsql';
 
 CREATE TRIGGER houses_table_modtime
