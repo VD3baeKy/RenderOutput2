012a7159391f010f432285a876d96b8218ffd68b
VD3baeKy <digfehc@gmail.com>
2025-05-17 07:17:10 +0900

    Create AllureSanityTest.java

---
+++ src/test/java/com/example/AllureSanityTest.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+public class AllureSanityTest {
+    @Test
+    public void allureHelloTest() {
+        System.out.println("Hello, Allure!");
+    }
+}
